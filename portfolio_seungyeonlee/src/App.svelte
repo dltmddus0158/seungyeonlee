de41b8b9c3e82edafc84cfd893a3757c571d098a
Seungyeon Lee <dltmddus0158@gmail.com>
2025-03-08 18:42:29 -0500

    modified nav bar, card section, added resume file

--- portfolio_seungyeonlee/src/App.svelte
+++ portfolio_seungyeonlee/src/App.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { preventDefault } from "svelte/legacy";
-
   function scrollToSection(id) {
     const section = document.getElementById(id);
     if (section) {
@@ -11,88 +9,32 @@
 
 <nav class="navbar">
   <ul class="nav-links">
-    <li><a href="#home">Seungyeon Lee</a></li>
+    <li><a href="#about" on:click|preventDefault={() => scrollToSection('about')}>About</a></li>
     <li><a href="#work" on:click|preventDefault={() => scrollToSection('work')}>Work</a></li>
     <li><a href="#writings" on:click|preventDefault={() => scrollToSection('writings')}>Writings</a></li>
-    <li><a href="#about" on:click|preventDefault={() => scrollToSection('about')}>About</a></li>
-    <li><a href="#resume" on:click|preventDefault={() => scrollToSection('resume')}>Resume</a></li>
-    <li><a href="#contact" on:click|preventDefault={() => scrollToSection('contact')}>Contact</a></li>
+    <li><a href="/public/resume.pdf" target="_blank">Resume</a></li>
   </ul>
 </nav>
 
 <main>
+  <section id="about" class="card">
+    <h3>ABOUT</h3>
+    <p>Hi, I am Seungyeon Lee.<p>
+    <p>Thanks for visiting my website.</p>
+
+  </section>
+
   <section id="work" class="card">
-    <h1>Work</h1>
-    <p>Your work content goes here.</p>
+    <h3>WORK</h3>
+    <p>Archives of my UX work</p>
     <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
+    <img src="/images/ex_img.jpg" width="300">
   </section>
 
   <section id="writings" class="card">
-    <h1>Writings</h1>
-    <p>Your writings content goes here.</p>
+    <h3>WRITINGS</h3>
+    <p>Archives of my blog posts</p>
     <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-  </section>
-
-  <section id="about" class="card">
-    <h1>About</h1>
-    <p>Your about content goes here.</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-  </section>
-
-  <section id="resume" class="card">
-    <h1>Resume</h1>
-    <p>Your resume content goes here.</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-  </section>
-
-  <section id="contact" class="card">
-    <h1>Contact</h1>
-    <p>Your contact content goes here.</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
+    <img src="/images/ex_img.jpg" width="300">
   </section>
 </main>
